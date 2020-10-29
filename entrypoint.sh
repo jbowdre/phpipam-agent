685f18a159396c447fbdd686ed6fcad479938d23
mc303 <daniel@buonocore.nl>
2020-10-29 18:09:23 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo \"$CRON_SCHEDULE /usr/local/bin/php /opt/phpipam-agent/index.php update > /proc/self/fd/1 2>/proc/self/fd/2\" > $CRONTAB_FILE
+chmod 0644 $CRONTAB_FILE
+
+exec "$@"
+
+
