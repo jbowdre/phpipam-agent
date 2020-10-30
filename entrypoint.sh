c0d39790e51682204efa772f4642cff5692904cd
mc303 <daniel@buonocore.nl>
2020-10-30 15:56:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+[ -z "$CRON_SCHEDULE" ] && CRON_SCHEDULE="1/15 * * * *"
+
 echo "$CRON_SCHEDULE /usr/local/bin/php /opt/phpipam-agent/index.php update > /proc/self/fd/1 2>/proc/self/fd/2" > $CRONTAB_FILE
 echo "$CRON_SCHEDULE /usr/local/bin/php /opt/phpipam-agent/index.php discover > /proc/self/fd/1 2>/proc/self/fd/2" >> $CRONTAB_FILE
 chmod 0644 $CRONTAB_FILE
