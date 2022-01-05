b75cad53366b825a9359ba7b4e68fafde2d4260b
John Bowdre <john@bowdre.net>
2022-01-05 15:18:24 -0600

    update cron schedule string

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-[ -z "$CRON_SCHEDULE" ] && CRON_SCHEDULE="1/15 * * * *"
+[ -z "$CRON_SCHEDULE" ] && CRON_SCHEDULE="*/15 * * * *"
 
 echo "$CRON_SCHEDULE /usr/local/bin/php /opt/phpipam-agent/index.php update > /proc/self/fd/1 2>/proc/self/fd/2" > $CRONTAB_FILE
 echo "$CRON_SCHEDULE /usr/local/bin/php /opt/phpipam-agent/index.php discover > /proc/self/fd/1 2>/proc/self/fd/2" >> $CRONTAB_FILE
